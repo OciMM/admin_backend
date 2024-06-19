56eb690efa636fab03942fe3622d3f1bacc6446b
OciMM <bekasovmaks20@gmail.com>
2024-06-19 15:19:29 +0300

    check views in notifications

--- storisbro_admin/notification/views.py
+++ storisbro_admin/notification/views.py
@@ -167,4 +167,4 @@ class NotificationToUserAPIView(APIView):
 
                 return Response({"message": "Данные успешно обработаны (Для всех)"}, status=status.HTTP_200_OK)
                 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors)
