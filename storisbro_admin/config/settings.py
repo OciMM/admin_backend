cf6f5ab9502d2de1b289ea8acc75f63e75cd1bb9
OciMM <bekasovmaks20@gmail.com>
2024-06-19 14:20:51 +0300

    test notifications

--- storisbro_admin/config/settings.py
+++ storisbro_admin/config/settings.py
@@ -116,6 +116,12 @@ DATABASES = {
         'PORT': 5432
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- storisbro_admin/notification/tests.py
+++ storisbro_admin/notification/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .services import send_message_email, send_message_for_all_email
 # Create your tests here.
+class ServicesTest(TestCase):
+
+    def test_is_leap_year(self):
+        send_message_email(email="jgots188@gmail.com", text="Тестировка от Django Test")
+        send_message_for_all_email(text="Тестировка от Django Test для всех")
